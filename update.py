e9c66cda40c59a564cfbe1d9d7565cdc8154742e
TheNooB <110566368+TheNooB4@users.noreply.github.com>
2023-06-11 23:09:57 +0000

    Update update.py

--- update.py
+++ update.py
@@ -1,8 +1,9 @@
 import os
 os.system("clear")
 os.system("git pull origin main")
-os.system("git checkout master")
+
 os.system("git fetch origin")
-os.system("git merge origin/master")
+os.("git pull")
+os.sytem("git pull origin master")
 
 
