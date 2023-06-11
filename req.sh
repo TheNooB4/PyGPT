7f6044f57c72d4fff7c45a428e2ac6f03575c55b
TheNooB <110566368+TheNooB4@users.noreply.github.com>
2023-06-11 19:09:16 +0000

    Add files via upload

---
+++ req.sh
@@ -0,0 +1,11 @@
+clear
+termux-setup-storage
+pip install openai
+pip install requests
+pip install pyfiglet
+pip install termcolor
+pip install pillow
+pip install colorama
+pip install setuptools
+clear
+echo -e "\e[36mWell Done!\e[0m Now run python Gpt.py"
